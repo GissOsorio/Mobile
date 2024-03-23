271d89625a7e4611f9eee02ecccae31ea1766b47
Gisela Osorio <giss_ot@hotmail.com>
2024-03-22 20:50:39 -0500

    Timer

--- app/src/main/java/com/example/mobile1/PersonList.kt
+++ app/src/main/java/com/example/mobile1/PersonList.kt
@@ -10,12 +10,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,10 +31,24 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
 
 
 @Composable
 fun PersonList() {
+    var timeLeft by remember {
+        mutableIntStateOf(10)
+    }
+    var isPaused by remember {
+        mutableStateOf(false)
+    }
+    LaunchedEffect(key1 = timeLeft) {
+        while (timeLeft > 0 &&!isPaused){
+            delay(1000L)
+            timeLeft --
+        }
+    }
     var nombre by remember {
         mutableStateOf("")
     }
@@ -48,6 +65,40 @@ fun PersonList() {
             .fillMaxSize()
             .padding(16.dp)
     ) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+        ){
+            Text(text = "Time Left: ${timeLeft.toString()}",
+                modifier = Modifier
+                    .padding(16.dp),
+                fontSize = 20.sp
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Button(
+                modifier = Modifier.padding(16.dp),
+                onClick = {
+                    isPaused = true
+            }) {
+                Icon(
+                    modifier = Modifier
+                        .size(20.dp),
+                    imageVector = Icons.Default.Warning,
+                    contentDescription = null
+                )
+            }
+            Button(
+                modifier = Modifier.padding(16.dp),
+                onClick = {
+                    timeLeft = 10
+            }) {
+                Icon(modifier = Modifier
+                    .size(20.dp),
+                    imageVector = Icons.Default.Refresh,
+                    contentDescription = null)
+
+            }
+        }
         Row (
             modifier = Modifier
                 .fillMaxWidth()
